06ccff5634b7ec60574f8cffa6a759c1c51f5d8f
jpared3s <jparedes.wall@gmail.com>
2023-03-10 17:00:12 -0500

    user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  
+
   namespace :admin do
     get 'categories/index'
     get 'categories/new'
@@ -27,6 +29,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories, only: [:index, :new, :create]
   end
+
+  resources :users, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
